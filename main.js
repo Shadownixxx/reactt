4c6b71071b11d5200bdcc6874085d85484e3f11d
krzysiek lakomy <krzysiek@gmail.com>
2019-05-14 16:16:47 +0200

    sdsd

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // const div = document.querySelector('div');
 
 // let divX = 250;
@@ -30,6 +29,7 @@
 //     }
 // })
 
+xcxx;
 
 // div.addEventListener("mouseup", () => {
 //     div.style.backgroundColor = "black";
@@ -69,7 +69,6 @@
 // const allEven = tabs.some(el => el % 2 === 0);
 // console.log(allEven);
 
-
 // const taab = [1, 2, 3, 4];
 // const result = taab.reduce((a, b) => a + b);
 // console.log(result);
@@ -88,28 +87,28 @@
 // console.log(btn.tagName);
 // console.log(btn.getAttribute('id'));
 
-const pierwszy = document.querySelector('.pierwszy');
+const pierwszy = document.querySelector(".pierwszy");
 pierwszy.style.backgroundColor = "green";
-const drugi = document.createElement('div');
+const drugi = document.createElement("div");
 pierwszy.appendChild(drugi);
-drugi.style.width = '50px';
-drugi.style.height = '50px';
+drugi.style.width = "50px";
+drugi.style.height = "50px";
 drugi.style.backgroundColor = "yellow";
-drugi.style.marginLeft = '20px'
+drugi.style.marginLeft = "20px";
 
 drugi.innerText = "działa";
-drugi.classList.add('drugi');
+drugi.classList.add("drugi");
 
-const div = document.querySelector(".div-test-replace")
+const div = document.querySelector(".div-test-replace");
 const btn = document.querySelector("#replaceTest");
 
-btn.addEventListener("click", function () {
-    const oldItem = div.querySelector("span");
+btn.addEventListener("click", function() {
+  const oldItem = div.querySelector("span");
 
-    const newItem = document.createElement("strong");
-    newItem.innerText = "Jestem nowym elementem";
+  const newItem = document.createElement("strong");
+  newItem.innerText = "Jestem nowym elementem";
 
-    div.replaceChild(newItem, oldItem);
+  div.replaceChild(newItem, oldItem);
 });
 
 // const element = document.querySelector('.button-move');
@@ -120,35 +119,37 @@ btn.addEventListener("click", function () {
 
 // ŁADNY PRZYCISK
 
-const element2 = document.querySelector('.button-test');
+const element2 = document.querySelector(".button-test");
 
-element2.addEventListener('mousemove', function (e) {
-    this.style.setProperty('--left', e.pageX - this.offsetLeft + "px");
-    this.style.setProperty('--top', e.pageY - this.offsetTop + "px");
-})
-
+element2.addEventListener("mousemove", function(e) {
+  this.style.setProperty("--left", e.pageX - this.offsetLeft + "px");
+  this.style.setProperty("--top", e.pageY - this.offsetTop + "px");
+});
 
 function Car(name, color) {
-    this.age = 0;
-    this.name = name;
-    this.color = color;
+  this.age = 0;
+  this.name = name;
+  this.color = color;
 
-    this.print = function () {
-        console.log(this.name + ' koloru ' + this.color);
-    }
+  this.print = function() {
+    console.log(this.name + " koloru " + this.color);
+  };
 }
 const maluch = new Car(" maluch", "rudy");
 maluch.print();
 
 const ob = {
-    time: 2000,
-    pets: ["pies", "kot", "chomik"],
-    print: function () {
-        setTimeout(function () {
-            for (const pet of this.pets) {
-                console.log(pet.toUpperCase());
-            }
-        }.bind(this), this.time)
-    }
-}
+  time: 2000,
+  pets: ["pies", "kot", "chomik"],
+  print: function() {
+    setTimeout(
+      function() {
+        for (const pet of this.pets) {
+          console.log(pet.toUpperCase());
+        }
+      }.bind(this),
+      this.time
+    );
+  }
+};
 ob.print();
